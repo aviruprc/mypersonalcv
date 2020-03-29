31dc99f3e01d52e7e35ee9b091aa034ac1e1ffb9
Avirup <aviruproychowdhury@gmail.com>
2020-03-29 18:31:24 +0530

    added javascript for submit

---
+++ public/js/contact.js
@@ -0,0 +1,26 @@
+const form = document.getElementById('contactform');
+
+form.addEventListener('submit', e => {
+
+  e.preventDefault();
+
+  const Title = document.getElementById('name').value;
+  const Description = document.getElementById('subject').value;
+  const Rating = document.getElementById('email').value;
+  const Cast = document.getElementById('message').value;
+
+  fetch('https://i9tsozqn90.execute-api.ap-south-1.amazonaws.com/add_movie/AddMovie01', {
+    headers: { 'Content-Type': 'application/json; charset=utf-8' },
+    method: 'POST',
+    body: JSON.stringify({
+        name: name,
+        subject:subject,
+        content: email,
+        email: message
+    })
+
+  }).then(() => {
+    form.reset();
+    alert('Thank you for Contacting');
+  });
+});
